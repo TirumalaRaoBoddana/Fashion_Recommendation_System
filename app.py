b5d1f60dc9920441d7abf63b85b96f3e35295bc7
BoddanaTirumala Rao <156399633+TirumalaRaoBoddana@users.noreply.github.com>
2026-01-03 14:34:41 +0530

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -59,12 +59,14 @@ def extract_image_embedding(pil_image, model):
 def load_data():
     download_files_from_gdrive()
 
-    image_embeddings = np.load("image_embeddings.npy")
-    image_names = np.load("image_names.npy", allow_pickle=True)
+    image_embeddings = np.load("image_embeddings.npy", allow_pickle=True)
+    image_names = np.load("image_names.npy", allow_pickle=True).astype(str)
     meta_data = pd.read_csv("mapped_meta_data.csv")
+    meta_data["image_name"] = meta_data["image_name"].astype(str)
     knn = joblib.load("knn_model.joblib")
 
     return image_embeddings, image_names, meta_data, knn
+
 
 
 def predict_info_from_neighbors(top_indices, metadata):
@@ -179,3 +181,4 @@ if __name__ == "__main__":
         st.subheader("Recommended Products")
 
         display_cards(recommendations, cards_per_row=5)
+
